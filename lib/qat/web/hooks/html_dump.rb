73953db9dc72d30864dc0546c028071398c0c445
Sérgio <sergio.matos@readinessit.com>
2021-05-07 12:34:04 +0100

    Removed attach of html dump on index html, the new data representation should be in other form, improve in  more recent cucumber versions

--- lib/qat/web/hooks/html_dump.rb
+++ lib/qat/web/hooks/html_dump.rb
@@ -15,7 +15,10 @@ After do |scenario|
   if scenario.failed?
     if QAT::Web::Exceptions::HTML_DUMP.any? { |exception| scenario.exception.kind_of?(exception) }
       # Embeds an existing HTML dump to Cucumber's HTML report
-      attach(QAT::Web::Browser::HTMLDump.take_html_dump, 'text/html')
+      # This attach the content of the file in this case the attach method appears it doesn't allow atm to embed a file link as before
+      # attach(QAT::Web::Browser::HTMLDump.take_html_dump, 'text/html')
+      # Only saving the content to the public folder
+       QAT::Web::Browser::HTMLDump.take_html_dump
     end
   end
 end
